24e37d95ee19b703ecff609ea8d2af2b69eccf8a
dmlwn31 <dmlwn31@gmail.com>
2021-02-26 18:09:07 +0900

    2/26 서울랜드 리뉴얼 완성

--- js/custom.js
+++ js/custom.js
@@ -14,21 +14,38 @@ function carousel() {
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
+// 메인 메뉴 네비게이션 마우스 호버 슬라이드
+// $(document).ready(function() {
+//   $("#gnb").mouseover(function() {
+//     $("#gnb").stop().slidedown("fast");
+//   });
+//   $("#gnb").mouseout(function() {
+//     $("#gnb").slideup("fast");
+//   });
+// });
+
+// mobile메뉴  slide
+$(document).ready(function(){
+  $(".mIcon").click(function(){
+    $(".mNavSlider").slideToggle();
+  });
+});
+
+
 //mobile menu click
 let mobileMenu = function(){
   $(".mIcon").click(function(){
     $(this).toggleClass("on");
     if($(this).hasClass("on")){
       $(this).find("i").attr("class", "fa fa-times");
-      // $(".navigation").slideDown("fast");
     } else {
       $(this).find("i").attr("class", "fa fa-bars");
-      // $(".navigation").slideup("fast");
     }
   });
 }
 
 mobileMenu();
+
   
 
 // 페이스북/블로그 탭 설정, active클래스 
